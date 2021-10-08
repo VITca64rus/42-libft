b488f5ef649921a7e175292010aa4bc89da9f724
Sarsfield Zelda <sazelda@at-n7.msk.21-school.ru>
2021-10-08 16:54:22 +0300

    add strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -8,28 +8,19 @@ char	*ft_strnstr(const char *havstack, const char *needle, size_t len)
 	i = 0;
 	if (len == 0)
 		return ((char *)havstack);
-	while (i < len)
+
+	while (havstack[i] != '\0' && i < len)
 	{
 		j = 0;
-		printf("%c %c", needle[j], havstack[i + j]);
-		while (needle[j] == havstack[i + j] && needle[j] != '\0')
+		while (needle[j] != '\0' && needle[j] == havstack[i + j])
+		{
 			j++;
-		printf("%d",j);
+		}
 		if (j == ft_strlen(needle))
 			return ((char *)&havstack[i]);
-		else
-			break;
+		else if (j != 0)
+			return ((void *)0);
 		i++;
 	}
 	return ((void *)0);
-}
-
-int main()
-{
-	char *s1 = "see FF your FF return FF now FF";
-	char *s2 = "FF";
-	size_t max = ft_strlen(s1);
-	//char *i1 = strnstr(s1, s2, max);
-	char *i2 = ft_strnstr(s1, s2, max);
-	printf("%s\n", i2);
 }
