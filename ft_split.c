053bdd196c453027d000c336bc19daf8a506c887
Sarsfield Zelda <sazelda@oa-h3.msk.21-school.ru>
2021-10-09 20:39:44 +0300

    add func get_count_words

--- ft_split.c
+++ ft_split.c
@@ -2,13 +2,35 @@
 
 static unsigned int	ft_count_word(char const *s, char c)
 {
-	
+	unsigned int	i;
+	unsigned int	res;
+
+	i = 0;
+	res = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		if (s[i - 1] == c && s[i] != c)
+			res++;
+		i++;
+	}
+	return (res);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	unsigned int	count_word;
+	char			**res;
 
 	count_word = ft_count_word(s, c);
-	return (res);
+	res = (char **)malloc(sizeof(char *) * (count_word + 1));
+	//return (res);
+}
+
+int main()
+{
+	char	a[] = " I d  am   Vikt  ";
+	printf("%d", ft_count_word(a, ' '));
+
 }
