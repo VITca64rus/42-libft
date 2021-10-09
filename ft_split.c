3f4b469156782d3ab000319b8303a74e0e76cf4c
Sarsfield Zelda <sazelda@oa-h3.msk.21-school.ru>
2021-10-09 20:20:39 +0300

    start split

---
+++ ft_split.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+static unsigned int	ft_count_word(char const *s, char c)
+{
+	
+}
+
+char	**ft_split(char const *s, char c)
+{
+	unsigned int	count_word;
+
+	count_word = ft_count_word(s, c);
+	return (res);
+}
